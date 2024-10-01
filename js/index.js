cf186da687adad0e16ca107a4ffdbc23b913dc47
hecmtzza <94489409+hecmtzza@users.noreply.github.com>
2024-10-01 11:00:49 -0600

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'I Have Something Te he escrito mucho estos días, solo no quiero que se llegue a una costumbre y por eso prefiero no darte aquello que escribo.Esto es una recapitulación de esas pequeñas cosas que escribí, no sin antes aprovechar para decirte que me siento pleno, me sorprende cómo es posible vivir tanto en tan poco, sentir nuevas cosas, encontrar una persona tan fuera de lo común.'.split('')
+const text = 'Tengo algo para ti, amor'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
